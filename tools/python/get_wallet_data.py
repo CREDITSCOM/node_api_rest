99bcecb8327b3942440fd4b1c391239fa90ba669
0xAAE <avramenko.a@gmail.com>
2020-03-17 15:16:38 +0300

    Updated GetBlocks toolset

--- tools/python/get_wallet_data.py
+++ tools/python/get_wallet_data.py
@@ -4,7 +4,7 @@ import json
 #url = 'http://192.168.0.27:8888/api/Monitor/GetWalletData/GetWalletData'
 #url = 'http://apinode.credits.com/api/Monitor/GetWalletData/GetWalletData'
 url =  'http://localhost:60476/api/Monitor/GetBlocks'
-#url =  'http://localhost:60476/api/Transaction/Execute'
+#url =  'http://localhost:60476/api/Monitor/GetWalletData'
 
 headers = {
     'Content-type': 'application/json'
@@ -15,11 +15,11 @@ headers = {
 data = {
     "authKey": "87cbdd85-b2e0-4cb9-aebf-1fe87bf3afdd"
     #, "MethodApi" : "GetWalletData222"
-    #, "PublicKey":"5B3YXqDTcWQFGAqEJQJP3Bg1ZK8FFtHtgCiFLT5VAxpe"
-    #, "NetworkAlias":"MainNet"
-    , "networkIp":"195.133.73.36"	# vh4 mainnet
+    , "PublicKey":"5B3YXqDTcWQFGAqEJQJP3Bg1ZK8FFtHtgCiFLT5VAxpe"
+    , "NetworkAlias":"MainNet"
+    #, "networkIp":"195.133.73.36"	# vh4 mainnet
     #, "networkIp":"165.22.242.197"	# do-lon4 testnet
-    , "networkPort":"9070"
+    #, "networkPort":"9070"
 	, "ConsensusInfo":False
 	, "Transactions":True
 	, "ContractsApproval":False
@@ -34,8 +34,8 @@ answer = requests.post(url, data=json.dumps(data), headers=headers)
 print(answer)
 if answer.status_code == 200:
     response = answer.json()
-    print(len(response))
-    print(response, flush=True)
+    print(response)
     text_file = open("sample.json", "w")
-    n = text_file.write(response)
+    print(response, file=text_file, flush=True)
+    #n = text_file.write(response)
     text_file.close()
