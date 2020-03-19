965ff5be94ada0c7065b08ede1343514183ac3c3
0xAAE <avramenko.a@gmail.com>
2020-03-19 11:37:04 +0300

    Add prod url of REST API

--- tools/python/get_wallet_data.py
+++ tools/python/get_wallet_data.py
@@ -3,7 +3,8 @@ import json
 
 #url = 'http://192.168.0.27:8888/api/Monitor/GetWalletData/GetWalletData'
 #url = 'http://apinode.credits.com/api/Monitor/GetWalletData/GetWalletData'
-url =  'http://localhost:60476/api/Monitor/GetBlocks'
+#url =  'http://localhost:60476/api/Monitor/GetBlocks'
+url =  'http://169.63.5.243:5006/api/Monitor/GetBlocks'
 #url =  'http://localhost:60476/api/Monitor/GetWalletData'
 
 headers = {
@@ -16,9 +17,9 @@ data = {
     "authKey": "87cbdd85-b2e0-4cb9-aebf-1fe87bf3afdd"
     #, "MethodApi" : "GetWalletData222"
     , "PublicKey":"5B3YXqDTcWQFGAqEJQJP3Bg1ZK8FFtHtgCiFLT5VAxpe"
-    #, "NetworkAlias":"MainNet"
+    , "NetworkAlias":"MainNet"
     #, "networkIp":"195.133.73.36"	# vh4 mainnet
-    , "networkIp":"165.22.242.197"	# do-lon4 testnet
+    #, "networkIp":"165.22.242.197"	# do-lon4 testnet
     , "networkPort":"9070"
 	, "ConsensusInfo":False
 	, "Transactions":True
