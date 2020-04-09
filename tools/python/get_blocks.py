4afe5d6a8b4bff2a86ce50c68333be48a46d3178
0xAAE <avramenko.a@gmail.com>
2020-04-09 11:29:49 +0300

    Update test tool

--- tools/python/get_blocks.py
+++ tools/python/get_blocks.py
@@ -3,9 +3,10 @@ import json
 
 #url = 'http://192.168.0.27:8888/api/Monitor/GetWalletData/GetWalletData'
 #url = 'http://apinode.credits.com/api/Monitor/GetWalletData/GetWalletData'
-url =  'http://localhost:60476/api/Monitor/GetBlocks'
+#url =  'http://localhost:60476/api/Monitor/GetBlocks'
 #url =  'http://169.63.5.243:5006/api/Monitor/GetBlocks'
 #url =  'http://localhost:60476/api/Monitor/GetWalletData'
+url = 'http://165.22.212.105:5000/api/Monitor/GetBlocks'
 
 headers = {
     'Content-type': 'application/json'
@@ -16,19 +17,19 @@ headers = {
 data = {
     "authKey": "87cbdd85-b2e0-4cb9-aebf-1fe87bf3afdd"
     #, "PublicKey":"5B3YXqDTcWQFGAqEJQJP3Bg1ZK8FFtHtgCiFLT5VAxpe"
-    , "NetworkAlias":"MainNet"
+    #, "NetworkAlias":"MainNet"
     #, "networkIp":"165.22.212.41"	# = TestNet
-    #, "networkPort":"9070"         # = TestNet
-    #, "networkIp":"195.133.73.36"	# vh4 mainnet
+    , "networkPort":"9070"         # executor
+    , "networkIp":"165.22.212.105"	# do6 mainnet
     #, "networkIp":"165.22.242.197"	# do-lon4 testnet
     #, "networkPort":"9070"
-	#, "ConsensusInfo":True
-	, "Transactions":True
+	, "ConsensusInfo":True
+	, "Transactions":False
 	#, "ContractsApproval":True
 	#, "Signatures":True
 	#, "Hashes":True
-	, "BeginSequence":239
-	, "EndSequence":239
+	, "BeginSequence":29352978
+	, "EndSequence":29352988
     }
 
 answer = requests.post(url, data=json.dumps(data), headers=headers)
